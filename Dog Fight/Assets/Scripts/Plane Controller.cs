db07c8bbfea04ab2b62c66ad3b1a15cf4cbdf76d
Sento920 <ruschta@uwec.edu>
2016-02-16 11:09:19 -0600

    commit of old things. work postponed until further notice, School work.

---
+++ Dog Fight/Assets/Scripts/Plane Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+    private Transform MousePos;
+    public float PlaneSpeed;
+    public float TurnSpeed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

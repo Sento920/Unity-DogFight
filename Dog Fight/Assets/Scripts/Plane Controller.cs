661c259b26a7241620466f8404a5daed46b58dad
Tyler Rusch <ruschta@uwec.edu>
2016-03-07 21:37:55 -0600

    Work starting.

--- Dog Fight/Assets/Scripts/Plane Controller.cs
+++ Dog Fight/Assets/Scripts/Plane Controller.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 
 public class PlaneController : MonoBehaviour {
-    private Transform MousePos;
+    private Vector3 MousePos;
     public float PlaneSpeed;
     public float TurnSpeed;
+    private float maxTurn;
     private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         rb = this.GetComponent<Rigidbody>();
-
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        MousePos = Input.mousePosition;
 	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(PlaneSpeed * Input.GetButton("Fire1"),0,0));
+        rb.
+    }
 }
